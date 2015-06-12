e217d55c0fb6f296873cf2976eea1de112307a21
rsamec <roman.samec2@gmail.com>
2015-06-12 20:12:36 +0200

    0.1.28

--- src/widgets/TextBoxInput.js
+++ src/widgets/TextBoxInput.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default class TextBoxInput extends React.Component{
-    render () {
-        var valueModel = this.props.valueLink;
-        var value = this.props.valueLink?valueModel.value:this.props.DefaultValue;
-        var handleChange = function (e) {
-            valueModel.value = e.target.value;
-        };
-        return (
-            <label>{this.props.label}
-                <input type='text' value={value} onChange={handleChange}/>
-            </label>
-        )
-    }
+	render () {
+		var valueModel = this.props.valueLink;
+		var value = this.props.valueLink?valueModel.value:this.props.DefaultValue;
+		var handleChange = function (e) {
+			valueModel.value = e.target.value;
+		};
+		return (
+			<label>{this.props.label}
+				<input type='text' value={value} onChange={handleChange}/>
+			</label>
+		)
+	}
 }
