b9018112365ac8c7c448535047a52dac4f9fc142
rsamec <roman.samec2@gmail.com>
2015-09-22 21:26:52 +0200

    remove react-intl dependency

--- src/widgets/JSXBox.js
+++ src/widgets/JSXBox.js
@@ -1,28 +1,5 @@
-'use strict';
-
 import styleFont from '../styles/font';
 var React = require('react');
-var BindToMixin = require('react-binding');
-var chainedFunction = require('../utils/createChainedFunction');
-var TangleNumberText = require('./input/TangleNumberText');
-var TangleBoolText = require('./input/TangleBoolText');
-
-
-
-var ReactIntl = require('react-intl');
-var IntlMixin = ReactIntl.IntlMixin;
-var FormattedNumber = ReactIntl.FormattedNumber;
-var FormattedDate = ReactIntl.FormattedDate;
-var FormattedTime = ReactIntl.FormattedTime;
-var FormattedRelative = ReactIntl.FormattedRelative;
-var FormattedMessage = ReactIntl.FormattedMessage;
-var FormattedHTMLMessage = ReactIntl.FormattedHTMLMessage;
-
-//shortcuts
-var TangleText = TangleNumberText;
-var BoolText = TangleBoolText;
-
-var _ = require('underscore');
 
 export default class JSXBox extends React.Component {
 	shouldComponentUpdate() {
